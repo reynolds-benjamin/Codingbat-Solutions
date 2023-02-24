9b32a55e5deef07556f79026e4b3989e71dbfc60
reynolds-benjamin <40626919@napier.ac.uk>
2023-02-24 17:38:34 +0000

    finish q5

--- Python/Warmup 1.py
+++ Python/Warmup 1.py
@@ -31,6 +31,11 @@ def diff21(n):
 #We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. 
 #We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return True if we are in trouble.
 
+def parrot_trouble(talking, hour):
+    if (hour > 20 or hour < 7) and talking:
+        return True
+    else: return False
+
 #Q6
 #Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
 
