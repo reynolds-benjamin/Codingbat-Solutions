e5ff5778bba54d600b2d472f116bf0d9eccc9dcf
reynolds-benjamin <40626919@napier.ac.uk>
2023-02-24 15:33:35 +0000

    adding warmup 2

--- Python/Warmup 1.py
+++ Python/Warmup 1.py
@@ -45,3 +45,45 @@ def makes10(a, b):
 
 def near_hundred(n):
     return ((110 >= n >= 90) or (210 >= n >= 190)) 
+
+#Q8
+#Given 2 int values, return True if one is negative and one is positive.
+#Except if the parameter "negative" is True, then return True only if both are negative.
+
+#Q9
+#Given a string, return a new string where "not " has been added to the front. 
+#However, if the string already begins with "not", return the string unchanged. 
+
+def not_string(str):
+    if str[:3] == "not":
+        return str
+    else: return 'not ' + str
+
+#Q10
+#Given a non-empty string and an int n, return a new string where the char at index n has been removed.
+#The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
+
+def missing_char(str, n):
+    ans = str.replace((str[n:n+1]), '')
+    return ans
+
+#Q11
+#Given a string, return a new string where the first and last chars have been exchanged.
+
+def front_back(str):
+    first = str[:1]
+    middle = str[1:-1]
+    last = str[-1:]
+
+    if len(str) <= 1:
+        return str
+    else:
+        return last + middle + first
+    
+#Q12
+#Given a string, we'll say that the front is the first 3 chars of the string. 
+#If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+
+def front3(str):
+    return str[:3] * 3
+
