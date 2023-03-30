97a1b0d8d031268e40dd4dc5b8b5e3afa2831b21
reynolds-benjamin <40626919@napier.ac.uk>
2023-03-30 17:10:15 +0100

    monkey question

--- Python/Warmup 1.py
+++ Python/Warmup 1.py
@@ -9,6 +9,9 @@ def sleep_in(weekday, vacation):
 #We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling.
 # We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
 
+def monkey_trouble(a_smile, b_smile):
+    return a_smile == b_smile;
+
 #Q3
 #Given two int values, return their sum. Unless the two values are the same, then return double their sum.
 
